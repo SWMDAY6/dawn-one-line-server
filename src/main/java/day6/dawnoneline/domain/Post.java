c0a43cba35489e6534fdbdea2322f0078d8aeea1
KimJyun <jhkim311@naver.com>
2022-10-13 20:54:54 +0900

    feat, refactor : Comment entity와 연관관계 매핑, Post entity 이름 수정 [DAY6-195]

---
+++ src/main/java/day6/dawnoneline/domain/Post.java
@@ -0,0 +1,73 @@
+package day6.dawnoneline.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "post")
+public class Post extends TimeStamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+    @Column
+    private String content;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private Double latitude;
+    @Column(nullable = false)
+    private Double longitude;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    public Post(String content, String password, Double latitude, Double longitude) {
+        this.content = content;
+        this.password = password;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCoordinate(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public void addComment(Comment comment) {
+        comments.add(comment);
+        comment.setPost(this);
+    }
+
+    public void addComments(Comment... comments) {
+        for (Comment comment : comments) {
+            this.addComment(comment);
+        }
+    }
+
+}
