248b6f2971e3ae43f71f4885a8b42a03a3d9fb10
KimJyun <jhkim311@naver.com>
2022-10-17 16:17:16 +0900

    fix : 예외 처리 코드 구현 [DAY6-195]

---
+++ src/main/java/day6/dawnoneline/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package day6.dawnoneline.exception;
+
+import javax.persistence.NoResultException;
+
+import org.hibernate.PropertyValueException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoResultException.class)
+    public String handleNoResultException(final NoResultException e) {
+        return "Post Id does not exist";
+    }
+
+    @ExceptionHandler(PropertyValueException.class)
+    public String handlePropertyValueException(final PropertyValueException e) {
+        return "not-null property references a null or transient value";
+    }
+}
