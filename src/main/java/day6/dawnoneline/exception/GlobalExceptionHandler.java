d6725e8f021103c99aee63f25b86a181249530f8
KimJyun <jhkim311@naver.com>
2022-10-19 15:15:24 +0900

    fix : Exception Handler 추가 [DAY6-195]

--- src/main/java/day6/dawnoneline/exception/GlobalExceptionHandler.java
+++ src/main/java/day6/dawnoneline/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package day6.dawnoneline.exception;
 import javax.persistence.NoResultException;
 
 import org.hibernate.PropertyValueException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,5 +22,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PropertyValueException.class)
     public String handlePropertyValueException(final PropertyValueException e) {
         return "not-null property references a null or transient value";
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public String handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+        return "Missing Request Parameter!";
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public String handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+        return "Please check the url again!";
     }
 }
