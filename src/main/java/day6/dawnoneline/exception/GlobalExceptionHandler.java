d28d10dd9f013e17aaeb246d356981fe98fc9909
KimJyun <jhkim311@naver.com>
2022-10-26 11:58:36 +0900

    fix : exception handler 추가 [DAY6-209]

--- src/main/java/day6/dawnoneline/exception/GlobalExceptionHandler.java
+++ src/main/java/day6/dawnoneline/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package day6.dawnoneline.exception;
 
+import java.util.NoSuchElementException;
+
 import javax.persistence.NoResultException;
 
 import org.hibernate.PropertyValueException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,12 +19,12 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoResultException.class)
     public String handleNoResultException(final NoResultException e) {
-        return "Input Id does not exist";
+        return "Result does not exist!";
     }
 
     @ExceptionHandler(PropertyValueException.class)
     public String handlePropertyValueException(final PropertyValueException e) {
-        return "not-null property references a null or transient value";
+        return "not-null property references a null or transient value!";
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
@@ -32,5 +35,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public String handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
         return "Please check the url again!";
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public String handleBadCredentialsException(final BadCredentialsException e) {
+        return "The password is wrong!";
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public String handleNoSuchElementException(final NoSuchElementException e) {
+        return "No Such Element!";
     }
 }
